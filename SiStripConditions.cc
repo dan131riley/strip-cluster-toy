9beed4ca476906a4d4bf59ec4b64bf0f2c0d78b9
Dan Riley <Daniel.Riley@cornell.edu>
2019-12-11 12:45:25 -0500

    portability

--- SiStripConditions.cc
+++ SiStripConditions.cc
@@ -2,6 +2,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <algorithm>
 
 struct FileChannel {
   fedId_t fedId_;
--- SiStripFEDBuffer.h
+++ SiStripFEDBuffer.h
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <cstddef>
 #include <array>
+#include <cstring>
 
 #include "FEDChannel.h"
 
